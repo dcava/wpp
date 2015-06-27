83d48d8b6ddfad6909e1b40b676edf44ca101314
David Cavallucci <david.cavallucci@gmail.com>
2015-06-27 23:22:48 +1000

    changed all km analysis to fully pooled/Rubin's rules analsyes extra utility functions

--- utilities.R
+++ utilities.R
@@ -21,3 +21,37 @@ mi.ci <- function(poolscalar) {
   lower <- round(df.ps$est - mult*sqrt(df.ps$var), 3)
   return(str_c("5yr OS: ", round(df.ps$est,3), " (95% CI ", lower, "-", upper, ")"))
 }
+
+lichtrubin <- function(fit){
+  ## pools the p-values of a one-sided test according to the Licht-Rubin method
+  ## this method pools p-values in the z-score scale, and then transforms back 
+  ## the result to the 0-1 scale
+  ## Licht C, Rubin DB (2011) unpublished
+  if (!is.mira(fit)) stop("Argument 'fit' is not an object of class 'mira'.")
+  fitlist <- fit$analyses
+  if (!inherits(fitlist[[1]], "htest")) stop("Object fit$analyses[[1]] is not an object of class 'htest'.")
+  m <- length(fitlist)
+  p <- rep(NA, length = m)
+  for (i in 1:m) p[i] <- fitlist[[i]]$p.value
+  z <- qnorm(p)  # transform to z-scale
+  num <- mean(z)
+  den <- sqrt(1 + var(z))
+  pnorm( num / den) # average and transform back
+}
+
+pool.km <- function(km.mira.object) {
+  results <- vector("list")
+    for (i in c("median", "LCL", "UCL")){
+      km.mira.object %>%
+        list.apply(summary) %>%
+        list.map(table) %>%
+        data.frame() %>%
+        select(contains(i)) %>%
+        rowMeans(na.rm = T) %>%
+        {
+          round(./365.25*12,1)
+        } -> results[[i]]
+    }
+  data.frame(results)
+  }
+
